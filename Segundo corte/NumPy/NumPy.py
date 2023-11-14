c4099a562ea7f0c55106b1c414e5acde935360bf
Esteffannia <101610634+Esteffannia@users.noreply.github.com>
2023-11-13 20:02:11 -0500

    Add files via upload

---
+++ Segundo corte/NumPy/NumPy.py
@@ -0,0 +1,9 @@
+# Se puede hacer referencia al paquete NumPy como np en lugar de numpy
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+# Comprobando la versión de NumPy
+print(np.__version__)
